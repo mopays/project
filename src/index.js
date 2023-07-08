f71b9f755f203bc8c713393eec45d4f9b0ad6dd9
mopays <75215491+mopays@users.noreply.github.com>
2023-07-08 18:51:51 +0700

    main
    home page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Itemsgrid from './components/itemsgrid';
+import Pagination from './components/Pagination';
+import Footer from './components/footer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Itemsgrid/>
+    <Pagination/>
+    <Footer />
   </React.StrictMode>
 );
 
